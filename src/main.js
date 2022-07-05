09dc588060042bbe83664e7203bd98d89dca5c44
Mr.Luo <1320264838@qq.com>
2022-07-05 08:40:21 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import BaiduMap from 'vue-baidu-map'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+Vue.use(BaiduMap,{
+  ak: 'i28SUEOuVYRl9oSbEu5pt9uPBLeAqshq'
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
